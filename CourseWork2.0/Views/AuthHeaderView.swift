734a0fef6816ee261cdb49ee144c3b0553eec0ac
Paul <pasha.makey@gmail.com>
2023-11-21 20:21:14 +0300

    Misc

---
+++ CourseWork2.0/Views/AuthHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  AuthHeaderView.swift
+//  CourseWork2.0
+//
+//  Created by Paul Makey on 21.11.23.
+//
+
+import UIKit
+
+class AuthHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
