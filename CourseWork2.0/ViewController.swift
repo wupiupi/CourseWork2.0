32a4ef168556b3385707f5fc13bbf1f0e220b54a
Paul <pasha.makey@gmail.com>
2023-11-21 18:50:13 +0300

    Initial Commit

---
+++ CourseWork2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CourseWork2.0
+//
+//  Created by Paul Makey on 21.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
