c7aff0ab2d7df718a9c74277826ea8be759de659
Paul <pasha.makey@gmail.com>
2023-11-21 19:22:47 +0300

    Initial setup

---
+++ CourseWork2.0/LoginController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginController.swift
+//  CourseWork2.0
+//
+//  Created by Paul Makey on 21.11.23.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
