f4790cbfd25aecd5a6aeb4847fd2567a5391368f
Paul <pasha.makey@gmail.com>
2023-11-22 15:15:44 +0300

    Added new View file: CustomTextField

--- CourseWork2.0/Views/CustomTextField.swift
+++ CourseWork2.0/Views/CustomTextField.swift
@@ -9,12 +9,43 @@ import UIKit
 
 class CustomTextField: UITextField {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    enum CustomTextFieldType {
+        case username
+        case email
+        case password
     }
-    */
-
+    
+    private let authFieldType: CustomTextFieldType
+    
+    init(fieldType: CustomTextFieldType) {
+        self.authFieldType = fieldType
+        super.init(frame: .zero)
+        
+        self.backgroundColor = .black
+        self.layer.cornerRadius = 10
+        
+        self.returnKeyType = .done
+        self.autocorrectionType = .no
+        self.autocapitalizationType = .none
+        
+        self.leftViewMode = .always
+        self.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: self.frame.size.height))
+        
+        switch fieldType {
+            case .username:
+                self.placeholder = "Username"
+            case .email:
+                self.placeholder = "Email address"
+                self.keyboardType = .emailAddress
+                self.textContentType = .emailAddress
+            case .password:
+                self.placeholder = "Password"
+                self.textContentType = .oneTimeCode
+                self.isSecureTextEntry = true
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
