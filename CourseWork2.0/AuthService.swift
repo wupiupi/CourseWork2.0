fe4a71941f0acb331530f5a8860c9d798ecfe8e0
Paul <pasha.makey@gmail.com>
2023-11-23 01:24:51 +0300

    Project changes

---
+++ CourseWork2.0/AuthService.swift
@@ -0,0 +1,8 @@
+//
+//  AuthService.swift
+//  CourseWork2.0
+//
+//  Created by Paul Makey on 23.11.23.
+//
+
+import Foundation
---
+++ CourseWork2.0/Models/RegisterUserRequest.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterUserRequest.swift
+//  CourseWork2.0
+//
+//  Created by Paul Makey on 23.11.23.
+//
+
+import Foundation
