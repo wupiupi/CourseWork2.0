952747050f9af5933f060fe368e6fb6f732190fe
Paul <pasha.makey@gmail.com>
2023-11-22 15:15:59 +0300

    Layout

--- CourseWork2.0/Views/AuthHeaderView.swift
+++ CourseWork2.0/Views/AuthHeaderView.swift
@@ -61,10 +61,19 @@ class AuthHeaderView: UIView {
         subTitleLabel.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            self.logoImageView.topAnchor.constraint(equalTo: self.layoutMarginsGuide.topAnchor,constant: 16),
+            self.logoImageView.topAnchor.constraint(equalTo: self.topAnchor,constant: 30),
             self.logoImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             self.logoImageView.widthAnchor.constraint(equalToConstant: 90),
-            self.logoImageView.heightAnchor.constraint(equalTo: logoImageView.widthAnchor)
+            self.logoImageView.heightAnchor.constraint(equalTo: logoImageView.widthAnchor),
+            
+            self.titleLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 19),
+            self.titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            
+            self.subTitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 12),
+            self.subTitleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.subTitleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            
         ])
     }
 }
