5b8e647e1e55348e1eb75805a50691db4d13bb54
Paul <pasha.makey@gmail.com>
2023-11-22 15:14:10 +0300

    Changes in init file

---
+++ CourseWork2.0/Views/CustomButton.swift
@@ -0,0 +1,20 @@
+//
+//  CustomButton.swift
+//  CourseWork2.0
+//
+//  Created by Paul Makey on 22.11.23.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CourseWork2.0/Views/CustomTextField.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTextField.swift
+//  CourseWork2.0
+//
+//  Created by Paul Makey on 22.11.23.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
