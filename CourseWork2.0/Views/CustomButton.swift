89f23116f3a3cb1e24eeeaf46e2aa5eade7145fc
Paul <pasha.makey@gmail.com>
2023-11-22 15:15:18 +0300

    new View file: CustomButton

--- CourseWork2.0/Views/CustomButton.swift
+++ CourseWork2.0/Views/CustomButton.swift
@@ -9,12 +9,34 @@ import UIKit
 
 class CustomButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    enum FontSize {
+        case big
+        case medium
+        case small
     }
-    */
-
+    
+    init(title: String, hasBackground: Bool, fontSize: FontSize) {
+        super.init(frame: .zero)
+        self.setTitle(title, for: .normal)
+        self.layer.cornerRadius = 12
+        self.layer.masksToBounds = true
+        
+        self.backgroundColor = hasBackground ? .systemGray4 : .clear
+        
+        let titleColor: UIColor = hasBackground ? .white : .systemGray
+        self.setTitleColor(titleColor, for: .normal )
+        
+        switch fontSize {
+            case .big:
+                self.titleLabel?.font = . systemFont(ofSize: 22, weight: .bold)
+            case .medium:
+                self.titleLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+            case .small:
+                self.titleLabel?.font = .systemFont(ofSize: 16, weight: .regular)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
