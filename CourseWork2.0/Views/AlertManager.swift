42bb42b0e3635f6a631e0f26c9a1008f814ede1c
Paul <pasha.makey@gmail.com>
2023-11-25 19:17:35 +0300

    New alert for an empty field

--- CourseWork2.0/Views/AlertManager.swift
+++ CourseWork2.0/Views/AlertManager.swift
@@ -10,10 +10,24 @@ import UIKit
 
 class AlertManager {
     
-    private static func showBasicAlert(on vc: UIViewController, title: String, message: String?) {
+    private static func showBasicAlert(
+        on vc: UIViewController,
+        title: String,
+        message: String?
+    ) {
         DispatchQueue.main.async {
-            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+            let alert = UIAlertController(
+                title: title,
+                message: message,
+                preferredStyle: .alert
+            )
+            alert.addAction(
+                UIAlertAction(
+                    title: "Dismiss",
+                    style: .default,
+                    handler: nil
+                )
+            )
             vc.present(alert, animated: true)
         }
     }
@@ -21,6 +35,14 @@ class AlertManager {
 
 // MARK: - Show Validation Alerts
 extension AlertManager {
+    
+    public static func showEmptyFieldAlert(on vc: UIViewController) {
+        self.showBasicAlert(
+            on: vc,
+            title: "Empty field",
+            message: "Please, enter field name"
+        )
+    }
     
     public static func showInvalidEmailAlert(on vc: UIViewController) {
         self.showBasicAlert(on: vc, title: "Invalid Email", message: "Please enter a valid email.")
